9e44aa27c87f39fc427c230053fb41cfb555b4ff
fpaarchitect <fpaarchitect@proton.me>
2025-02-19 15:07:46 +0100

    astro

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+export default {
+	"tailwindStylesheet": "./src/styles/global.css",
+	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+	overrides: [
+		{
+			files: "*.astro",
+			options: {
+				parser: "astro",
+			},
+		},
+	],
+};
